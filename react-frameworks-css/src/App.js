6aed074f7c8c7e7a5272cfd57c63da47dcc896f2
hector-mendoza <hec1702@gmail.com>
2022-08-24 16:21:37 -0500

    CSS Frameworks (Bootstrap React - Material UI)

--- react-frameworks-css/src/App.js
+++ react-frameworks-css/src/App.js
@@ -1,14 +1,19 @@
 import React from 'react';
-import Bootstrap from './components/Bootstrap';
-import Bulma from './components/Bulma';
 
+// React CSS Frameworks
+import MaterialUI from './components/MaterialUI';
+// import ReactBootstrap from './components/ReactBootstrap';
+
+// Normal CSS Frameworks
+// import Bulma from './components/Bulma';
+// import Bootstrap from './components/Bootstrap';
 
 function App() {
   return (
     <div>
-      <h1>CSS Frameworks with React</h1>
-      {/* <Bootstrap /> */}
-      <Bulma />
+      <h1>CSS Frameworks of React</h1>
+      {/* <ReactBootstrap /> */}
+      <MaterialUI />
     </div>
   );
 }
---
+++ react-frameworks-css/src/components/MaterialUI.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import MaterialCard from './MaterialCard';
+import MaterialDrawer from './MaterialDrawer';
+
+export default function MaterialUI() {
+    return (
+        <>
+            <h2>
+                MaterialUI
+            </h2>
+            <MaterialCard />
+            <MaterialDrawer />
+        </>
+    )
+}
