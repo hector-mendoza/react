33a0e5c0ac0ea571cda86194636e68651c151fe3
hector-mendoza <hec1702@gmail.com>
2022-10-10 17:22:39 -0500

    Modal: Funcionalidad y reutilización componente

--- crud/src/components/Modal.js
+++ crud/src/components/Modal.js
@@ -1,10 +1,14 @@
 import "./Modal.css";
 
-const Modal = ({ children }) => {
+const Modal = ({ children, isOpen, closeModal }) => {
+
+    // to avoid closing modal when clicking on the article/modal container
+    const handleModalContainerClick = e => { e.stopPropagation(); };
+
     return (
-        <article className="modal is-open">
-            <div className="modal-container">
-                <button className="modal-close">CLOSE</button>
+        <article className={`modal ${isOpen && "is-open"}`} onClick={closeModal}>
+            <div className="modal-container" onClick={handleModalContainerClick}>
+                <button className="modal-close" onClick={closeModal}>CLOSE</button>
                 {children}
             </div>
         </article>
--- crud/src/components/Modals.js
+++ crud/src/components/Modals.js
@@ -1,29 +1,29 @@
 import React from 'react';
 import { useModal } from '../hooks/useModal';
 import Modal from './Modal';
+import ContactForm from './ContactForm';
 
 const Modals = () => {
 
     // only for Modal 1
     const [isOpenModal1, openModal1, closeModal1] = useModal(false);
 
-    // only for Modal 2
-    const [isOpenModal2, openModal2, closeModal2] = useModal(false);
+    // only for Modal Contact
+    const [isOpenModalForm, openModalForm, closeModalForm] = useModal(false);
 
     return (
         <div>
             <h2>Modals</h2>
-            <button>Modal 1</button>
-            <Modal>
+            <button onClick={openModal1}>Modal 1</button>
+            <Modal isOpen={isOpenModal1} closeModal={closeModal1}>
                 <h3>Modal 1</h3>
                 <p>Hello, this is the content for Modal 1</p>
                 <img src='https://via.placeholder.com/300.png/09f/fffC/O' alt='animals' />
             </Modal>
-            {/* <Modal>
-                <h3>Modal 2</h3>
-                <p>Hello, this is the content for Modal 2</p>
-                <img src='https://via.placeholder.com/200.png/09f/fffC/O' alt='animals' />
-            </Modal> */}
+            <button onClick={openModalForm}>Modal Form</button>
+            <Modal isOpen={isOpenModalForm} closeModal={closeModalForm}>
+                <ContactForm />
+            </Modal>
         </div>
     );
 }
