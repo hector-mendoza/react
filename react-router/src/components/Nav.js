a7a4b9728816b4273a21bbe64a3f45f02c08dfc8
hector-mendoza <hec1702@gmail.com>
2022-10-18 17:30:58 -0500

    React Router: Link & NavLink

---
+++ react-router/src/components/Nav.js
@@ -0,0 +1,21 @@
+import { Link, NavLink } from "react-router-dom";
+
+const Nav = () => {
+    return (
+        <nav>
+            {/* <Link to="/">Home</Link>
+            <Link to="/about">About</Link>
+            <Link to="/contact">Contact</Link> */}
+
+            {/* NavLinks*/}
+
+            {/* we add the prop => end,  to ensure this component isn't matched as "active" when its descendant paths are matched. */}
+            <NavLink to="/" end>Home</NavLink>
+
+            <NavLink to="/about">About</NavLink>
+            <NavLink to="/contact">Contact</NavLink>
+        </nav>
+    );
+}
+
+export default Nav;
