56d096050ca18bfd904fa538eed8930786997d80
hector-mendoza <hec1702@gmail.com>
2022-11-04 08:23:44 -0600

    React Router: HashRouter

--- react-router/src/components/BasicConcepts.js
+++ react-router/src/components/BasicConcepts.js
@@ -1,5 +1,5 @@
 // por comodidad se le asigna el alias de Router
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate, HashRouter, Link } from "react-router-dom";
 
 // import every page/component
 import Home from "../pages/Home";
@@ -14,10 +14,51 @@ import Login from "../pages/Login";
 import Dashboard from "../pages/Dashboard";
 import PrivateRoute from "../components/PrivateRoute";
 
+
 // SWITCH was replaced by ROUTES
 const BasicConcepts = () => {
     return (
         <>
+            {/* HashRouter */}
+            {/* It's very easy to use and functional when you don't have a server side rendering for changing URL routes */}
+            {/* https://dominio.com/#/recursoABuscar */}
+            <h2>Hash Router</h2>
+            <HashRouter>
+                <nav>
+                    {/* on Hash Router you don't need to type the "#", it will get auto-added */}
+                    <Nav />
+                    {/* <Link to="/">Home</Link>
+                    <Link to="/about">About</Link>
+                    <Link to="/contact">Contact</Link> */}
+                </nav>
+
+                {/* Routes inside a HashRouter so the navigation works */}
+                <Routes>
+                    <Route index element={<Home />} />
+                    <Route path="/about" element={<About />} />
+                    <Route path="/contact" element={<Contact />} />
+                    <Route path="/products" element={<Products />} />
+
+                    {/* Friendly URL */}
+                    <Route path="/user/:username/:age" element={<User />} />
+
+                    {/* Redirections */}
+                    <Route path="/acerca" element={<Navigate to="/about" />} />
+                    <Route path="/contacto" element={<Navigate to="/contact" />} />
+
+                    {/* Rutas Anidadas/Nested Routes */}
+                    {/* Route to every React topic (*) */}
+                    <Route path="/react/*" element={<ReactTopics />} />
+
+                    {/* Private Routes */}
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/dashboard" element={<PrivateRoute component={Dashboard} />} />
+
+                    <Route path="*" element={<Error404 />} />
+
+                </Routes>
+            </HashRouter>
+            <hr />
             <h2>Basic Concepts</h2>
             <div>
                 <Router>
