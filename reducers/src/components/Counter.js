e95c60222be0096711ca12ba10f3e0b490cdafc8
hector-mendoza <hec1702@gmail.com>
2022-12-13 08:43:52 -0600

    Reducers - Payload

--- reducers/src/components/Counter.js
+++ reducers/src/components/Counter.js
@@ -2,13 +2,30 @@ import { useReducer  } from "react";
 
 const initialState = { count : 0 };
 
+const TYPES = {
+    INCREMENT: "INCREMENT",
+    INCREMENT_5: "INCREMENT_5",
+    DECREMENT: "DECREMENT",
+    DECREMENT_5: "DECREMENT_5",
+    RESET: "RESET"
+};
+
 function reducer (state, action) {
     switch (action.type) {
-        case "INCREMENT":
+        case TYPES.INCREMENT:
             return { count: state.count + 1 }
 
-        case "DECREMENT":
+        case TYPES.INCREMENT_5:
+            return { count: state.count + action.payload }
+
+        case TYPES.DECREMENT:
             return { count: state.count - 1 }
+
+        case TYPES.DECREMENT_5:
+            return { count: state.count - action.payload }
+
+        case TYPES.RESET:
+            return { count: initialState }
 
         default:
             return state;
@@ -21,15 +38,23 @@ const Counter = () => {
     // Hook useReducer
     const [state, dispatch] = useReducer(reducer, initialState);
 
-    const add = () => dispatch({ type: "INCREMENT" });
-    const remove = () => dispatch({ type: "DECREMENT" });
+    // type => defines the action type that the REDUCER function will follow
+    // payload => will be the new value => action on the REDUCER function
+    const add = () => dispatch({ type: TYPES.INCREMENT });
+    const add_5 = () => dispatch({ type: TYPES.INCREMENT_5, payload: 5 });
+    const remove = () => dispatch({ type: TYPES.DECREMENT });
+    const remove_5 = () => dispatch({ type: TYPES.DECREMENT_5, payload: 5 });
+    const reset = () => dispatch({ type: TYPES.RESET });
 
     return (
         <div style={{textAlign: "center"}}>
             <h2>Counter - Reducer</h2>
             <nav>
                 <button onClick={add}>+</button>
+                <button onClick={add_5}>+5</button>
                 <button onClick={remove}>-</button>
+                <button onClick={remove_5}>-5</button>
+                <button onClick={reset}>RESET</button>
             </nav>
             <h3>{ state.count }</h3>
         </div>
